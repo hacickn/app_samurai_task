9687f9023bc5d786d24bebafe54beed1dbcc1e2a
Hacı Çakın <haci.cakin@ug.bilkent.edu.tr>
2022-05-04 13:13:21 +0300

    Readme is created and comments are added.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const axios = require( "axios" )
+
 const url1 =
     "https://prod-storyly-media.s3.eu-west-1.amazonaws.com/test-scenarios/sample_1.json";
 const url2 =
@@ -9,31 +10,31 @@ const url3 =
 async function fetch( url ) {
     return await axios
         .request( {
-            url: url, // change url parameter respectively!
-            method: "GET",
+            url: url,                               // Its path
+            method: "GET",                          // It is GET request
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json"  // Accept only json types
             },
         } )
         .then( async function ( response ) {
-            if ( response.status === 200 ) {
+            if ( response.status === 200 ) {        // If response status is equal to 200, then return the data
                 return response.data
-            } else {
+            } else {                                // If response status is not equal to 200, then show the current status
                 console.log( response.status + " code is taken!" )
                 return null
             }
         } )
         .catch( async function ( error ) {
-            console.log( error.message )
+            console.log( error.message )            // If error is caught by function, show it
             return null
         } );
 }
 
 function calculatePrice( data ) {
-    if ( data.price !== undefined ) {
+    if ( data.price !== undefined ) {               // if price is not undefined then it means we can directly calculate
         return data.count * data.price;
-    } else {
-        let total = 0
+    } else {                                        // if price is undefined then it means we should first calculate its
+        let total = 0                               // price.
         data.items.forEach( ( item ) => {
             total = total + calculatePrice( item )
         } )
@@ -45,15 +46,20 @@ function calculatePrice( data ) {
 function getAndCalculate( url ) {
     fetch( url )
         .then( data => {
-            if ( data ) {
+            if ( data ) {                           // if data exist
                 console.log( "< " + url + " >  => " + calculatePrice( data ) )
-            } else {
+            } else {                                // if data is not exist
                 console.log( "Data can not be taken!" )
             }
         } )
 }
 
+// first request
 getAndCalculate( url1 )
+
+// second request
 getAndCalculate( url2 )
+
+// third request
 getAndCalculate( url3 )
 
---
+++ readme.md
@@ -0,0 +1,28 @@
+# APP SAMURAI INTERNSHIP TASK
+
+* Implementation was done by using node environment. 
+* Node version is v17.6.0
+* The only additional package is Axios that its version is v0.23.0.
+
+
+---
+### To run the application, follow these:
+
+1) Clone the repository to your local computer.
+2) In repository folder open terminal and run the following command:
+```
+npm install
+```
+3) After packages are loaded, run the command below:
+```
+node index.js
+```
+
+---
+**IMPORTANT NOTE**: Since _Axios_ works asynchronous, output order can be any order
+
+
+###
+**Hacı Çakın**
+######
+**Bilkent University Computer Science Student**
